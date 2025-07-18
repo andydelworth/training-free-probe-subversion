a219ccab322b03491ae8a997f3711772ae5ca3af
AndyDelworth <andrew_delworth@brown.edu>
2025-07-17 21:55:43 -0700

    add id field; precompute activation script

--- precompute_activations.py
+++ precompute_activations.py
@@ -4,12 +4,14 @@ import json
 import torch
 import transformers
 from transformers import AutoTokenizer, AutoModelForCausalLM
+from freeze_dried_data import WFDD
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--data_name', default='Monkeys')
 args = parser.parse_args()
 
 data_name = args.data_name
+data_folder = f'data/{data_name}'
 
 '''
 this function defines where the vector representation of the text comes from
@@ -30,4 +32,24 @@ model = AutoModelForCausalLM.from_pretrained(
     model_name, torch_dtype=torch.bfloat16, device_map="auto"
 )
 
-print(get_activation(model, tok, text))
+'''
+load strings from specified json file
+'''
+with open(os.path.join(os.path.join(data_folder, 'data.json')), 'r') as f:
+    data = json.load(f)
+
+if os.path.exists(os.path.join(data_folder, 'activations.fdd')):
+    raise ValueError(f'Activations already exist for {data_name}')
+
+activations = WFDD(os.path.join(data_folder, 'activations.fdd'))
+
+for item in data:
+    text = item['text']
+    datum_id = item['id']
+    activation = get_activation(model, tok, text)
+    activations[datum_id] = activation.cpu().numpy()
+
+activations.close()
+    
+    
+
