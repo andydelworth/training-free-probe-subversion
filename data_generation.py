ee099b6059080835b7ebf20e280cf924fd022a9c
root <root@trn85.phx15.castle.fm>
2025-07-18 04:16:35 +0000

    data gen works

--- data_generation.py
+++ data_generation.py
@@ -1,3 +1,6 @@
+import json
+import csv
+from tqdm import tqdm
 import random
 import transformers
 import torch
@@ -5,7 +8,7 @@ import argparse
 import os
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--N', default=1000)
+parser.add_argument('--N', default=100)
 parser.add_argument('--target_concept', default='Monkeys')
 args = parser.parse_args()
 
@@ -15,6 +18,7 @@ pipeline = transformers.pipeline(
     "text-generation",
     model=model_id,
     model_kwargs={"torch_dtype": torch.bfloat16},
+    device_map="auto"
 )
 
 target_concept = args.target_concept
@@ -23,6 +27,11 @@ output_dir = f'data/{target_concept_formatted}'
 os.makedirs(output_dir, exist_ok=True)
 concept_list = open('data/concepts.txt').readlines()
 concept_list = [c.strip() for c in concept_list]
+
+system_prompt = "Write something that connects the concept(s) the user specifies. Your writing should be short. \
+Randomly select your writing style - usually just regular writing, but sometimes it can also be informal, narrative, \
+poem, or others, or just random unstructured text. It MUST be about the concepts listed. Typos, slang or other \
+errors are allowed."
 
 '''
 data generation v1
@@ -64,27 +73,47 @@ neg_concepts = []
 
 for i, count in enumerate(counts):
     for _ in range(count):
-        concepts = [target_concept] + random.sample(concept_list, i)
+        concepts = random.sample(concept_list, i + 1)
         random.shuffle(concepts)
-        pos_concepts.append(templates[i].format(*concepts[:i+1]))
+        neg_concepts.append(templates[i].format(*concepts[:i+1]))
 
 pos_messages = [
-    [{"role": "system", "content": "Write something that involves all of the concepts listed in the user message"},
+    [{"role": "system", "content": system_prompt},
     {"role": "user", "content": pos_concept}]
     for pos_concept in pos_concepts
 ]
 
 neg_messages = [
-    [{"role": "system", "content": "Write something that involves all of the concepts listed in the user message"},
+    [{"role": "system", "content": system_prompt},
     {"role": "user", "content": neg_concept}]
     for neg_concept in neg_concepts
 ]
 
-outputs = pipeline(
-    pos_messages[:5],
-    max_new_tokens=512,
-)
+pipeline.tokenizer.pad_token = pipeline.tokenizer.eos_token
 
-breakpoint()
 
-print(outputs[0]["generated_text"][-1])
+def batched_generate(messages, label, pipe, batch_size=32):
+    outputs = []
+    for i in tqdm(range(0, len(messages), batch_size)):
+        batch = messages[i:i + batch_size]
+        out = pipe(batch, max_new_tokens=128)          # out: list[list[dict]]
+        for seqs in out:                               # seqs is a list
+            outputs.append([seqs[0]["generated_text"][-1]['content'], label])
+    return outputs
+
+pos_outputs = batched_generate(pos_messages, 1, pipeline)
+pos_data = [pos_output + [pos_concept] for pos_output, pos_concept in zip(pos_outputs, pos_concepts)]
+neg_outputs = batched_generate(neg_messages, 0, pipeline)
+neg_data = [neg_output + [neg_concept] for neg_output, neg_concept in zip(neg_outputs, neg_concepts)]
+
+
+with open(os.path.join(output_dir, 'data.json'), "w") as f:
+    json_data = []
+    for row in pos_data + neg_data:
+        json_data.append({
+            'text': row[0],
+            'label': row[1],
+            'concepts': row[2]
+        })
+
+    json.dump(json_data, f)
--- train_probe.py
+++ train_probe.py
@@ -1,9 +1,13 @@
+import random
 import torch
 import argparse
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--data_name', default='Monkeys')
+args = parser.parse_args()
+
 class ProbeDataset(torch.utils.data.Dataset):
-    def __init__(self, dataset_folder, is_train=True):
-        self.dataset_folder = dataset_folder
+    def __init__(self, data):
         self.is_train = is_train
         # this file stores list of (text, activations, label)
         self.dataset = torch.load(f"{dataset_folder}/{'train' if is_train else 'test'}.pt")
@@ -15,9 +19,15 @@ class ProbeDataset(torch.utils.data.Dataset):
         return self.dataset[idx][1], self.dataset[idx][2]
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--dataset_folder", type=str, required=True)
-args = parser.parse_args()
+def train_test_split(data, pct=.2):
+    random.shuffle(data)
+    # train, test
+    return data[:int((1-pct)*len(data))], data[int((1-pct)*len(data)):]
 
 dataset_folder = args.dataset_folder
+
+with open(os.path.join(f'data/{dataset_folder}/data.json'), 'r') as f:
+    data = json.load(f)
+
+
 
